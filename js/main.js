be214b8223f49ff387a3eb61919297a411d6c93c
orionpax00 <durgesh123.iitr@gmail.com>
2019-10-15 13:26:49 +0530

    files deleted

--- js/main.js
+++ js/main.js
@@ -18,40 +18,9 @@ $(document).ready(function(){
     $(".menu-bar").on('click', function(e){
         e.preventDefault();
         $("nav").toggleClass('hide');
-        $("span", this).toggleClass("lnr-menu lnr-cross");
+        $("i", this).toggleClass("fa-bars fa-times");
         $(".main-menu").addClass('mobile-menu');
     });
 
-    $('select').niceSelect();
-    $('.img-pop-up').magnificPopup({
-        type: 'image',
-        gallery:{
-        enabled:true
-        }
-    });
-
-    $('.active-bottle-carousel').owlCarousel({
-        items:1,
-        loop:true,
-        nav: false,
-        autoplay: true,
-        autoplayTimeout:3000,
-        autoplayHoverPause:true
-    });
-
-    $('.play-btn').magnificPopup({
-        disableOn: 700,
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-        fixedContentPos: false
-    });
-
-
-    $(document).ready(function() {
-        $('#mc_embed_signup').find('form').ajaxChimp();
-    });
-    // -------   Mail Send ajax
 
  });
